95926009254a19d9503e3c320c17acf1dfe392c9
Gesiel Rios <gesielrios@gmail.com>
2021-04-30 11:21:29 -0300

    Adding the license for project and a small description in the README; Implementation of test cases for the view.

--- poketest/core/tests.py
+++ poketest/core/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse
+import json
 
-# Create your tests here.
+class CoreViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.success_response_for_pokemon_ditto = json.dumps({
+            "imposter": "This Pokémon transforms into a random opponent upon entering battle.  This effect is identical to the move transform.",
+            "limber": "This Pokémon cannot be paralyzed.\n\nIf a Pokémon is paralyzed and acquires this ability, its paralysis is healed; this includes when regaining a lost ability upon leaving battle."
+        })
+        cls.failure_response_for_pokemon_ditto = json.dumps({'message': 'Pokemon not found or not specified.'})
+
+    def test_load_index_page(self):    
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/index.html')
+    
+    def test_success_endpoint(self):    
+        response = self.client.get('/api/pokemon/ditto')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(
+            json.dumps(response.json()), 
+            self.success_response_for_pokemon_ditto
+        )
+    
+    def test_failure_endpoint(self):    
+        response = self.client.get('/api/pokemon/JohnDoe')
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(
+            json.dumps(response.json()), 
+            self.failure_response_for_pokemon_ditto
+        )
